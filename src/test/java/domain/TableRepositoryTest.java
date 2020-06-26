fc0db2ec2ea57580eac622e0ffd438b452dd2f0a
K.S.KIM <nullable@kakao.com>
2020-06-26 14:02:27 +0900

    feat: 테이블 단건 조회 메서드 추가

---
+++ src/test/java/domain/TableRepositoryTest.java
@@ -0,0 +1,34 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class TableRepositoryTest {
+    private TableRepository tableRepository;
+
+    @BeforeEach
+    void setUp() {
+        tableRepository = new TableRepository();
+    }
+
+    @DisplayName("모든 테이블을 반환")
+    @Test
+    void findAll() {
+        assertThat(tableRepository.findAll()).isNotNull();
+    }
+
+    @DisplayName("테이블 번호로 테이블을 찾음")
+    @Test
+    void findMenuByNumber() {
+        assertThat(tableRepository.findTableByNumber(1)).isNotEmpty();
+    }
+
+    @DisplayName("테이블 번호가 일치하는 테이블이 존재하지 않는 경우 빈 옵셔널 반환")
+    @Test
+    void findMenuByNumber_FindNotExistNumber_ReturnEmptyOptional() {
+        assertThat(tableRepository.findTableByNumber(50)).isEmpty();
+    }
+}
