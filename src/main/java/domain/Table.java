ca5ced7e7d0af10f45e8691281c9b090e6a6df30
K.S.KIM <nullable@kakao.com>
2020-06-26 13:59:36 +0900

    feat: Table 검증로직 및 메서드 추가
    * 생성할 수 있는 테이블인지 여부를 확인하는 메서드 추가
    * 테이블 번호와 일치하는 번호인지 확인하는 메서드 추가
    * 테스트 코드 추가

--- src/main/java/domain/Table.java
+++ src/main/java/domain/Table.java
@@ -1,10 +1,24 @@
 package domain;
 
 public class Table {
+    private static final int MIN_NUMBER = 1;
+
     private final int number;
 
     public Table(final int number) {
+        validateNumber(number);
         this.number = number;
+    }
+
+    private void validateNumber(final int number) {
+        if (number < MIN_NUMBER) {
+            throw new IllegalArgumentException("테이블 번호는 양수만 입력 가능합니다.\n" +
+                    "입력된 수: " + number);
+        }
+    }
+
+    public boolean isSameNumber(final int number) {
+        return this.number == number;
     }
 
     @Override
---
+++ src/test/java/domain/TableTest.java
@@ -0,0 +1,39 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class TableTest {
+    @DisplayName("테이블 생성")
+    @Test
+    void constructor() {
+        assertThat(new Table(1)).isInstanceOf(Table.class);
+    }
+
+    @DisplayName("테이블 번호가 양수가 아닌 경우 예외 발생")
+    @Test
+    void constructor_NumberIsNotPositive_ExceptionThrown() {
+        assertThatThrownBy(() -> new Table(0))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("테이블 번호는 양수만 입력 가능합니다");
+    }
+
+    @DisplayName("입력받은 수와 테이블 번호가 같으면 true 반환")
+    @Test
+    void isSameNumber_NumberIsSame_ReturnTrue() {
+        Table table = new Table(1);
+
+        assertThat(table.isSameNumber(1)).isTrue();
+    }
+
+    @DisplayName("입력받은 수와 테이블 번호가 다르면 false 반환")
+    @Test
+    void isSameNumber_NumberIsNotSame_ReturnFalse() {
+        Table table = new Table(1);
+
+        assertThat(table.isSameNumber(2)).isFalse();
+    }
+}
